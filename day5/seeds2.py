5b4c6bb78984a1fc2dce358c283e325741c31769
Bjorn Wictorin <bjorn.wictorin@gmail.com>
2023-12-06 23:11:05 +0100

    day5: part 2
    Cleaner code. Takes appr. 4.5 minutes to run.

--- day5/seeds2.py
+++ day5/seeds2.py
@@ -4,14 +4,12 @@ from re import findall
 from typing import List, Tuple
 
 def remap_entry(entry: int, table: List[Tuple[int, int, int]]) -> int:
-    # print(f"{table=}")
     new_entry = entry # by default values map to themselves
     for src_start, dst_start, range_len in table:
         if src_start <= entry < src_start + range_len:
             new_entry = dst_start + (entry - src_start)
             break
     return new_entry
-
 
 def apply_all_tables(entry: int, tables: List[List[Tuple[int, int, int]]]) -> int:
     for table in tables:
@@ -35,29 +33,26 @@ def main():
                 translation_table[-1].append(numbers)
             else:
                 assert len(line) == 1 # only newline
-    print(f"{seeds=}")
+
+    # create ranges for seeds
+    seed_ranges = []
+    range_start = 0
+    for ii, val in enumerate(seeds):
+        if ii % 2 == 0:
+            range_start = val
+        else:
+            range_end = range_start + val
+            seed_ranges.append((range_start, range_end))
+    assert len(seed_ranges) == len(seeds) // 2
 
     # search backwards, from location to seed
     translation_table = translation_table[::-1]
-
     location = -1
-    seed_found = False
-    while not seed_found:
+    while True:
         location += 1
         seed = apply_all_tables(location, translation_table)
-        # if seed in seeds:
-        #     seed_found = True
-        if location % 10000000 == 0:
-            print(location)
-        range_start = 0
-        for ii, val in enumerate(seeds):
-            if ii % 2 == 0:
-                range_start = val
-            else:
-                range_end = range_start + val
-                if seed in range(range_start, range_end):
-                    seed_found = True
-                    break
+        if any(range_start <= seed < range_end for range_start, range_end in seed_ranges):
+            break
     print(location)
 
 
